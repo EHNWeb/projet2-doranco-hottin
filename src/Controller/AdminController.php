4a1845a6b8a7d5b7490366a48064015e799c2c2a
HOTTIN Eric <70805292+EHNWeb@users.noreply.github.com>
2022-08-24 15:34:40 +0200

    Add AdminController AdminVehicules

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Vehicule;
+use App\Repository\VehiculeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +16,23 @@ class AdminController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+        return $this->render('admin/index.html.twig');
+    }
+
+    /**
+     * @Route("/admin/vehicules", name="admin_vehicules")
+     */
+    public function adminVehicules(VehiculeRepository $repo, EntityManagerInterface $manager): Response
+    {
+        // Récupération des nomd des champs de la table VEHICULE
+        $chapmsTableVehicule = $manager->getClassMetadata(Vehicule::class)->getFieldNames();
+
+        // On récupère tous les véhicules
+        $vehicules = $repo->findAll();
+
+        return $this->render('admin/admin_vehicules.html.twig', [
+            'vehicules' => $vehicules,
+            'colonnes' => $chapmsTableVehicule
         ]);
     }
 }
