ba1549a5608bee51e782d9a7db8f22c3e398c9fb
HOTTIN Eric <70805292+EHNWeb@users.noreply.github.com>
2022-08-25 16:15:46 +0200

    Setup Authentication and Login

--- src/Entity/Membre.php
+++ src/Entity/Membre.php
@@ -4,11 +4,13 @@ namespace App\Entity;
 
 use App\Repository\MembreRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=MembreRepository::class)
+ * @UniqueEntity(fields={"pseudo"}, message="There is already an account with this pseudo")
  */
 class Membre implements UserInterface, PasswordAuthenticatedUserInterface
 {
