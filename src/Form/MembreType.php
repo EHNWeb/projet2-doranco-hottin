1444b3d6117cf8788ed138c553766c5401da05b4
HOTTIN Eric <70805292+EHNWeb@users.noreply.github.com>
2022-08-25 09:57:46 +0200

    Update Show Vehicule

---
+++ src/Form/MembreType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Membre;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MembreType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('pseudo')
+            ->add('mdp')
+            ->add('nom')
+            ->add('prenom')
+            ->add('email')
+            ->add('civilite')
+            ->add('statut')
+            ->add('date_enregistrement')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Membre::class,
+        ]);
+    }
+}
