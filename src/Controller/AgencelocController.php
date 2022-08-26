23a69106ab8f4993ae8382ff5b17143698ffe06d
HOTTIN Eric <70805292+EHNWeb@users.noreply.github.com>
2022-08-26 09:54:16 +0200

    Add Catalogue Vehicule Home Page

--- src/Controller/AgencelocController.php
+++ src/Controller/AgencelocController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Commande;
 use App\Form\SearchVehiculeType;
 use App\Repository\CommandeRepository;
+use App\Repository\VehiculeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,10 +17,11 @@ class AgencelocController extends AbstractController
     /**
      * @Route("/", name="app_agenceloc")
      */
-    public function index(CommandeRepository $repo, Request $superGlobals): Response
+    public function index(VehiculeRepository $repoVehicule, EntityManagerInterface $manager, Request $superGlobals): Response
     {
         // Création d'un objet commande
         $commande = new Commande();
+        $vehicules = $repoVehicule->findAll();
 
         // Création du formulaire en suivant la structure de SearchVehiculeType #}
         $form = $this->createForm(SearchVehiculeType::class, $commande);
@@ -29,6 +32,7 @@ class AgencelocController extends AbstractController
 
         return $this->render('agenceloc/index.html.twig', [
             'formCommande' => $form->createView(),
+            'vehicules' => $vehicules
         ]);
     }
 }
