80d15cfeb0512561dcf77a91deb9604520ea4edd
HOTTIN Eric <70805292+EHNWeb@users.noreply.github.com>
2022-08-26 18:54:46 +0200

    Fix Problem Logout

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -27,7 +27,7 @@ class SecurityController extends AbstractController
     }
 
     /**
-     * @Route("/logout", name="app_logout")
+     * @Route("/logout", name="logout")
      */
     public function logout(): void
     {
