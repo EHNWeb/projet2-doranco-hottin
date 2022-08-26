864af67a5833ca013cb599308d9062c3b54d0b12
HOTTIN Eric <70805292+EHNWeb@users.noreply.github.com>
2022-08-26 17:45:27 +0200

    Update @Assert in Membre, Vehicule and Comande

--- src/Entity/Vehicule.php
+++ src/Entity/Vehicule.php
@@ -2,10 +2,11 @@
 
 namespace App\Entity;
 
-use App\Repository\VehiculeRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use App\Repository\VehiculeRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * @ORM\Entity(repositoryClass=VehiculeRepository::class)
